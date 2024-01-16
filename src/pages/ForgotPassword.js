592c59e4c9101d4d9f3fddcf8183e2c885d964e9
Benson306 <bnkimtai@gmail.com>
2024-01-16 17:08:41 +0300

    Passord reset Done

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,13 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import ImageLight from '../assets/img/forgot-password-office.jpeg'
 import ImageDark from '../assets/img/forgot-password-office-dark.jpeg'
 import { Label, Input, Button } from '@windmill/react-ui'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ForgotPassword() {
+  const [email, setEmail] = useState(null);
+
+  const handleSubmit = () => {
+    if(email == null ){
+      toast.error('Enter Email', {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
+      return
+    }
+
+    fetch(`${process.env.REACT_APP_API_URL}/reset_admin_password`,{
+      method: 'POST',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        email
+      })
+    })
+    .then(res =>{
+      if(res.ok){
+        toast.success('Password has been sent to your email. Login in with it.', {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
+      }else{
+        toast.error('Check credential', {
+          position: "top-right",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
+      }
+    })
+    .catch(err => {
+      toast.error('Server Error', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+    })
+
+  }
+
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+      <ToastContainer />
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div className="flex flex-col overflow-y-auto md:flex-row">
           <div className="h-32 md:h-auto md:w-1/2">
@@ -32,12 +101,28 @@ function ForgotPassword() {
 
               <Label>
                 <span>Email</span>
-                <Input className="mt-1" placeholder="Jane Doe" />
+                <Input type="email" onChange={e => setEmail(e.target.value)} className="mt-1" placeholder="janedoe@gmail.com" />
               </Label>
 
-              <Button tag={Link} to="/login" block className="mt-4">
+              <Button 
+              class="bg-blue-600 p-2 rounded-lg text-sm text-white w-full mt-4 text-center" block
+              onClick={()=>{
+                handleSubmit();
+              }}
+              >
                 Recover password
               </Button>
+
+              <hr className="my-6" />
+
+              <p >
+                <Link
+                  className="text-sm font-medium text-blue-600 dark:text-blue-400 hover:underline"
+                  to="/login"
+                >
+                  Go back to login?
+                </Link>
+              </p>
             </div>
           </main>
         </div>
